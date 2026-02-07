a465bcd572136f74f2fdd6382ff8c54d2412168a
Priya Dharshini <priya260511@gmail.com>
2026-02-07 12:54:04 +0530

    Initial commit: backend code

---
+++ src/main/java/com/password/repository/CredentialRepository.java
@@ -0,0 +1,7 @@
+package com.password.repository;
+
+import com.password.model.Credential;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CredentialRepository extends JpaRepository<Credential, Long> {
+}
